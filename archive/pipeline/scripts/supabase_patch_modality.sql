01f147576f0e6a790239bef43b664c5baaa3ea1d
orphanai2026 <orphan.ai2026@gmail.com>
2026-07-21 11:23:29 +0300

    Delete archive/pipeline directory

--- archive/pipeline/scripts/supabase_patch_modality.sql
+++
@@ -1,6 +0,0 @@
--- Normalize modality codes to atlas.html expected values
-update public.atlas_images set modality = 'MRI' where modality in ('MR','mr','Mr');
-update public.atlas_images set modality = 'CT'  where modality in ('ct','Ct');
-
--- Verify
-select modality, count(*) from public.atlas_images group by modality;
